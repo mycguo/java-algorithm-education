306cc0d97942147b69cb97fbedc505eb7161986e
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2008-07-23 20:58:30 +0000

    enum

---
+++ algorithm/src/edu/effective/java/OperationSymbol.java
@@ -0,0 +1,18 @@
+package edu.effective.java;
+
+public enum OperationSymbol {
+	
+	
+	PLUS("+") {double apply(double x, double y) {return x+y;} },
+	MINUS("-") {double apply(double x, double y) {return x-y;} },
+	TIMES("*") {double apply(double x, double y) {return x*y;} },
+	DIVIDE("/") {double apply(double x, double y) {return x/y;} };
+	
+	private final String symbol;
+	private OperationSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	@Override public String toString() { return symbol; }
+	abstract double apply (double x, double y);
+
+}
