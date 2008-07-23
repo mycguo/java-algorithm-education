f9fcae0c0a711e2292a9f869a1e17234e8c53c9f
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2008-07-23 20:58:30 +0000

    enum

---
+++ trunk/algorithm/src/edu/effective/java/Operation.java
@@ -0,0 +1,12 @@
+package edu.effective.java;
+
+public enum Operation {
+	
+	PLUS {double apply(double x, double y) {return x+y;} },
+	MINUS {double apply(double x, double y) {return x-y;} },
+	TIMES {double apply(double x, double y) {return x*y;} },
+	DIVIDE {double apply(double x, double y) {return x/y;} };
+	
+	abstract double apply (double x, double y);
+
+}
