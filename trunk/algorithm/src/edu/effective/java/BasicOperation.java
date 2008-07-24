17978a01091499d9732f824120e13f3f8feb2565
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2008-07-24 01:11:00 +0000

    Extend Enum

---
+++ trunk/algorithm/src/edu/effective/java/BasicOperation.java
@@ -0,0 +1,15 @@
+package edu.effective.java;
+
+public enum BasicOperation implements Operations {
+	PLUS("+") {public double apply(double x, double y) {return x+y;} },
+	MINUS("-") {public double apply(double x, double y) {return x-y;} },
+	TIMES("*") {public double apply(double x, double y) {return x*y;} },
+	DIVIDE("/") {public double apply(double x, double y) {return x/y;} };
+	
+	private final String symbol;
+	private BasicOperation(String symbol) {
+		this.symbol = symbol;
+	}
+	@Override public String toString() { return symbol; }
+	
+}
---
+++ trunk/algorithm/src/edu/effective/java/Operations.java
@@ -0,0 +1,5 @@
+package edu.effective.java;
+
+public interface Operations {
+	double apply(double x, double y);
+}
