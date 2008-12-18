33aa4016287c3d547e83a19d421d425a29b99f1f
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2008-12-18 01:36:16 +0000

    Digester

---
+++ algorithm/src/edu/java/regex/TestRegex.java
@@ -0,0 +1,36 @@
+package edu.java.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/*
+ * This is from SUN
+ * http://java.sun.com/docs/books/tutorial/essential/regex/index.html
+ */
+public class TestRegex {
+    public static void main(String[] args){
+    	Scanner scanner = new Scanner(System.in);
+        while (true) {
+        	System.out.println("\nEnter your regex: ");
+
+            Pattern pattern = 
+            Pattern.compile(scanner.nextLine());
+
+            System.out.println("Enter input string:");
+            Matcher matcher = 
+            pattern.matcher(scanner.nextLine());
+
+            boolean found = false;
+            while (matcher.find()) {
+                System.out.println( String.format("I found the text \"%s\" starting at " +
+                   "index %d and ending at index %d.%n",
+                    matcher.group(), matcher.start(), matcher.end()));
+                found = true;
+            }
+            if(!found){
+            	System.out.println("No match found.%n");
+            }
+        }
+    }
+
+}
