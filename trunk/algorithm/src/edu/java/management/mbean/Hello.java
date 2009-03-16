b452f084170fbb406ce3ffb253af1fcee8bfbc62
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2009-03-16 18:07:07 +0000

    JMX

--- trunk/algorithm/src/edu/java/management/mbean/Hello.java
+++ trunk/algorithm/src/edu/java/management/mbean/Hello.java
@@ -1,6 +1,11 @@
 package edu.java.management.mbean;
 
-public class Hello implements HelloMBean {
+import javax.management.AttributeChangeNotification;
+import javax.management.MBeanNotificationInfo;
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
+
+public class Hello extends NotificationBroadcasterSupport implements HelloMBean {
 	   private String message = null;
 
 	   public Hello() {
@@ -12,8 +17,36 @@ public class Hello implements HelloMBean {
 	   }
 
 	   public void setMessage(String message) {
+		  String oldMessage = getMessage();
 	      this.message = message;
+	      
+	      Notification n = 
+	            new AttributeChangeNotification(this, 
+						    sequenceNumber++, 
+						    System.currentTimeMillis(), 
+						    "CacheSize changed", 
+						    "message", 
+						    "String", 
+						    oldMessage, 
+						    this.message); 
+	 
+		sendNotification(n); 
+
 	   }
+	   
+	    @Override 
+	    public MBeanNotificationInfo[] getNotificationInfo() { 
+	        String[] types = new String[] { 
+	            AttributeChangeNotification.ATTRIBUTE_CHANGE 
+	        }; 
+	        String name = AttributeChangeNotification.class.getName(); 
+	        String description = "An attribute of this MBean has changed"; 
+	        MBeanNotificationInfo info = 
+	            new MBeanNotificationInfo(types, name, description); 
+	        return new MBeanNotificationInfo[] {info}; 
+	    } 
+	 
+
 
 	   public String getMessage() {
 	      return message;
@@ -22,4 +55,6 @@ public class Hello implements HelloMBean {
 	   public void sayHello() {
 	      System.out.println(message);
 	   }
+	   
+	   int sequenceNumber =0;
 	}
