624581246512196741da437770be913f23020746
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2009-04-14 23:19:14 +0000

    Annotation

--- trunk/algorithm/src/edu/java/annotation/GroupToDo.java
+++ trunk/algorithm/src/edu/java/annotation/GroupToDo.java
@@ -6,8 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-/**
- * http://www.ibm.com/developerworks/library/j-annotate2.html
+/*
+ * * http://www.ibm.com/developerworks/library/j-annotate2.html 
+ * Data members in
+ * annotation types are set up to work using limited information. You don't
+ * define a member variable and then provide accessor and mutator methods.
+ * Instead, you define a single method, named after the member, that you want to
+ * allow for. The data type should be the return value of the method.
  */
 public @interface GroupToDo {
 
