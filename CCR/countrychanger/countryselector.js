55cd391920889d522cb62083f61ca4f29ec1c35e
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2011-03-24 17:36:31 +0000

    prettyprint

--- CCR/countrychanger/countryselector.js
+++ CCR/countrychanger/countryselector.js
@@ -1,117 +1,251 @@
-//-------------------------------------------
-// include country selector
-//-------------------------------------------
-<!--
-document.write(''
-
-
-+'<ul class="dropdown-box">'
+<ul class="dropdown-box">'
 +'	<li lang="en" xml:lang="en" class="dropdown">A through B'
 +'		<ul lang="en" xml:lang="en" class="dropdown-hidebox">'
-+'			<li><a href="http://nwa.fujitsu.com/">Algeria</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Anguilla</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Aruba</a></li>'
-+'			<li><a href="http://au.fujitsu.com/">Australia</a></li>'
-+'			<li><a href="http://at.fujitsu.com/">Austria</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Bahamas</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Barbados</a></li>'
-+'			<li><a href="http://be.fujitsu.com/">Belgium</a></li>'
-+'			<li><a href="http://br.fujitsu.com/">Brazil</a></li>'
-+'			<li><a href="http://bg.fujitsu.com/">Bulgaria</a></li>'
++'			<li>
+				<a href="http://nwa.fujitsu.com/">Algeria</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Anguilla</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Aruba</a>
+			</li>'
++'			<li>
+				<a href="http://au.fujitsu.com/">Australia</a>
+			</li>'
++'			<li>
+				<a href="http://at.fujitsu.com/">Austria</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Bahamas</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Barbados</a>
+			</li>'
++'			<li>
+				<a href="http://be.fujitsu.com/">Belgium</a>
+			</li>'
++'			<li>
+				<a href="http://br.fujitsu.com/">Brazil</a>
+			</li>'
++'			<li>
+				<a href="http://bg.fujitsu.com/">Bulgaria</a>
+			</li>'
 +'		</ul>'
 +'	</li>'
 +'	<li lang="en" xml:lang="en" class="dropdown">C through F'
 +'		<ul lang="en" xml:lang="en" class="dropdown-hidebox">'
-+'			<li><a href="http://ca.fujitsu.com/en/">Canada (English)</a></li>'
-+'			<li><a href="http://ca.fujitsu.com/fr/">Canada (French)</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Cayman Islands</a></li>'
-+'			<li><a href="http://cn.fujitsu.com/">China</a></li>'
-+'			<li><a href="http://hr.fujitsu.com/">Croatia</a></li>'
-+'			<li><a href="http://cz.fujitsu.com/">Czech Republic</a></li>'
-+'			<li><a href="http://dk.fujitsu.com/">Denmark</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Dominica</a></li>'
-+'			<li><a href="http://ee.fujitsu.com/">Estonia</a></li>'
-+'			<li><a href="http://fi.fujitsu.com/">Finland</a></li>'
-+'			<li><a href="http://fr.fujitsu.com/">France</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">French Guiana</a></li>'
++'			<li>
+				<a href="http://ca.fujitsu.com/en/">Canada (English)</a>
+			</li>'
++'			<li>
+				<a href="http://ca.fujitsu.com/fr/">Canada (French)</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Cayman Islands</a>
+			</li>'
++'			<li>
+				<a href="http://cn.fujitsu.com/">China</a>
+			</li>'
++'			<li>
+				<a href="http://hr.fujitsu.com/">Croatia</a>
+			</li>'
++'			<li>
+				<a href="http://cz.fujitsu.com/">Czech Republic</a>
+			</li>'
++'			<li>
+				<a href="http://dk.fujitsu.com/">Denmark</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Dominica</a>
+			</li>'
++'			<li>
+				<a href="http://ee.fujitsu.com/">Estonia</a>
+			</li>'
++'			<li>
+				<a href="http://fi.fujitsu.com/">Finland</a>
+			</li>'
++'			<li>
+				<a href="http://fr.fujitsu.com/">France</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">French Guiana</a>
+			</li>'
 +'		</ul>'
 +'	</li>'
 +'	<li lang="en" xml:lang="en" class="dropdown">G through I'
 +'		<ul lang="en" xml:lang="en" class="dropdown-hidebox">'
-+'			<li><a href="http://de.fujitsu.com/">Germany</a></li>'
-+'			<li><a href="http://gr.fujitsu.com/">Greece</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Guadeloupe</a></li>'
-+'			<li><a href="http://hk.fujitsu.com/">Hong Kong</a></li>'
-+'			<li><a href="http://hu.fujitsu.com/">Hungary</a></li>'
-+'			<li><a href="http://in.fujitsu.com/">India</a></li>'
-+'			<li><a href="http://id.fujitsu.com/">Indonesia</a></li>'
-+'			<li><a href="http://ie.fujitsu.com/">Ireland</a></li>'
-+'			<li><a href="http://it.fujitsu.com/">Italy</a></li>'
++'			<li>
+				<a href="http://de.fujitsu.com/">Germany</a>
+			</li>'
++'			<li>
+				<a href="http://gr.fujitsu.com/">Greece</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Guadeloupe</a>
+			</li>'
++'			<li>
+				<a href="http://hk.fujitsu.com/">Hong Kong</a>
+			</li>'
++'			<li>
+				<a href="http://hu.fujitsu.com/">Hungary</a>
+			</li>'
++'			<li>
+				<a href="http://in.fujitsu.com/">India</a>
+			</li>'
++'			<li>
+				<a href="http://id.fujitsu.com/">Indonesia</a>
+			</li>'
++'			<li>
+				<a href="http://ie.fujitsu.com/">Ireland</a>
+			</li>'
++'			<li>
+				<a href="http://it.fujitsu.com/">Italy</a>
+			</li>'
 +'		</ul>'
 +'	</li>'
 +'	<li lang="en" xml:lang="en" class="dropdown">J through M'
 +'		<ul lang="en" xml:lang="en" class="dropdown-hidebox">'
-+'			<li><a href="http://caribbean.fujitsu.com/">Jamaica</a></li>'
-+'			<li><a href="http://jp.fujitsu.com/">Japan</a></li>'
-+'			<li><a href="http://kr.fujitsu.com/">Korea</a></li>'
-+'			<li><a href="http://lt.fujitsu.com/">Lithuania</a></li>'
-+'			<li><a href="http://lu.fujitsu.com/">Luxembourg</a></li>'
-+'			<li><a href="http://my.fujitsu.com/">Malaysia</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Martinique</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Montserrat</a></li>'
-+'			<li><a href="http://nwa.fujitsu.com/">Morocco</a></li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Jamaica</a>
+			</li>'
++'			<li>
+				<a href="http://jp.fujitsu.com/">Japan</a>
+			</li>'
++'			<li>
+				<a href="http://kr.fujitsu.com/">Korea</a>
+			</li>'
++'			<li>
+				<a href="http://lt.fujitsu.com/">Lithuania</a>
+			</li>'
++'			<li>
+				<a href="http://lu.fujitsu.com/">Luxembourg</a>
+			</li>'
++'			<li>
+				<a href="http://my.fujitsu.com/">Malaysia</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Martinique</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Montserrat</a>
+			</li>'
++'			<li>
+				<a href="http://nwa.fujitsu.com/">Morocco</a>
+			</li>'
 +'		</ul>'
 +'	</li>'
 +'	<li lang="en" xml:lang="en" class="dropdown">N through R'
 +'		<ul lang="en" xml:lang="en" class="dropdown-hidebox">'
-+'			<li><a href="http://nl.fujitsu.com/">Netherlands</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Netherlands Antilles</a></li>'
-+'			<li><a href="http://nz.fujitsu.com/">New Zealand</a></li>'
-+'			<li><a href="http://no.fujitsu.com/">Norway</a></li>'
-+'			<li><a href="http://ph.fujitsu.com/">Philippines</a></li>'
-+'			<li><a href="http://pl.fujitsu.com/">Poland</a></li>'
-+'			<li><a href="http://pt.fujitsu.com/">Portugal</a></li>'
-+'			<li><a href="http://ro.fujitsu.com/">Romania</a></li>'
-+'			<li><a href="http://ru.fujitsu.com/">Russia</a></li>'
++'			<li>
+				<a href="http://nl.fujitsu.com/">Netherlands</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Netherlands Antilles</a>
+			</li>'
++'			<li>
+				<a href="http://nz.fujitsu.com/">New Zealand</a>
+			</li>'
++'			<li>
+				<a href="http://no.fujitsu.com/">Norway</a>
+			</li>'
++'			<li>
+				<a href="http://ph.fujitsu.com/">Philippines</a>
+			</li>'
++'			<li>
+				<a href="http://pl.fujitsu.com/">Poland</a>
+			</li>'
++'			<li>
+				<a href="http://pt.fujitsu.com/">Portugal</a>
+			</li>'
++'			<li>
+				<a href="http://ro.fujitsu.com/">Romania</a>
+			</li>'
++'			<li>
+				<a href="http://ru.fujitsu.com/">Russia</a>
+			</li>'
 +'		</ul>'
 +'	</li>'
 +'	<li lang="en" xml:lang="en" class="dropdown">S'
 +'		<ul lang="en" xml:lang="en" class="dropdown-hidebox">'
-+'			<li><a href="http://caribbean.fujitsu.com/">Saint Vincent and Grenadines</a></li>'
-+'			<li><a href="http://rs.fujitsu.com/">Serbia</a></li>'
-+'			<li><a href="http://sg.fujitsu.com/">Singapore</a></li>'
-+'			<li><a href="http://sk.fujitsu.com/">Slovakia</a></li>'
-+'			<li><a href="http://si.fujitsu.com/">Slovenia</a></li>'
-+'			<li><a href="http://za.fujitsu.com/">South Africa</a></li>'
-+'			<li><a href="http://es.fujitsu.com/">Spain</a></li>'
-+'			<li><a href="http://se.fujitsu.com/">Sweden</a></li>'
-+'			<li><a href="http://ch.fujitsu.com/">Switzerland</a></li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Saint Vincent and Grenadines</a>
+			</li>'
++'			<li>
+				<a href="http://rs.fujitsu.com/">Serbia</a>
+			</li>'
++'			<li>
+				<a href="http://sg.fujitsu.com/">Singapore</a>
+			</li>'
++'			<li>
+				<a href="http://sk.fujitsu.com/">Slovakia</a>
+			</li>'
++'			<li>
+				<a href="http://si.fujitsu.com/">Slovenia</a>
+			</li>'
++'			<li>
+				<a href="http://za.fujitsu.com/">South Africa</a>
+			</li>'
++'			<li>
+				<a href="http://es.fujitsu.com/">Spain</a>
+			</li>'
++'			<li>
+				<a href="http://se.fujitsu.com/">Sweden</a>
+			</li>'
++'			<li>
+				<a href="http://ch.fujitsu.com/">Switzerland</a>
+			</li>'
 +'		</ul>'
 +'	</li>'
 +'	<li lang="en" xml:lang="en" class="dropdown">T'
 +'		<ul lang="en" xml:lang="en" class="dropdown-hidebox">'
-+'			<li><a href="http://tw.fujitsu.com/">Taiwan</a></li>'
-+'			<li><a href="http://th.fujitsu.com/en/">Thailand (English)</a></li>'
-+'			<li><a href="http://th.fujitsu.com/">Thailand (Thai )</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Trinidad and Tobago</a></li>'
-+'			<li><a href="http://nwa.fujitsu.com/">Tunisia</a></li>'
-+'			<li><a href="http://tr.fujitsu.com/">Turkey</a></li>'
++'			<li>
+				<a href="http://tw.fujitsu.com/">Taiwan</a>
+			</li>'
++'			<li>
+				<a href="http://th.fujitsu.com/en/">Thailand (English)</a>
+			</li>'
++'			<li>
+				<a href="http://th.fujitsu.com/">Thailand (Thai )</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Trinidad and Tobago</a>
+			</li>'
++'			<li>
+				<a href="http://nwa.fujitsu.com/">Tunisia</a>
+			</li>'
++'			<li>
+				<a href="http://tr.fujitsu.com/">Turkey</a>
+			</li>'
 +'		</ul>'
 +'	</li>'
 +'	<li lang="en" xml:lang="en" class="dropdown">U through V'
 +'		<ul lang="en" xml:lang="en" class="dropdown-hidebox">'
-+'			<li><a href="http://middleeast.fujitsu.com/">United Arab Emirates</a></li>'
-+'			<li><a href="http://uk.fujitsu.com/">United Kingdom</a></li>'
-+'			<li><a href="http://us.fujitsu.com/">United States</a></li>'
-+'			<li><a href="http://vn.fujitsu.com/">Vietnam (English)</a></li>'
-+'			<li><a href="http://vn.fujitsu.com/vn/">Vietnam (Vietnamese)</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Virgin Islands British</a></li>'
-+'			<li><a href="http://caribbean.fujitsu.com/">Virgin Islands U.S.</a></li>'
++'			<li>
+				<a href="http://middleeast.fujitsu.com/">United Arab Emirates</a>
+			</li>'
++'			<li>
+				<a href="http://uk.fujitsu.com/">United Kingdom</a>
+			</li>'
++'			<li>
+				<a href="http://us.fujitsu.com/">United States</a>
+			</li>'
++'			<li>
+				<a href="http://vn.fujitsu.com/">Vietnam (English)</a>
+			</li>'
++'			<li>
+				<a href="http://vn.fujitsu.com/vn/">Vietnam (Vietnamese)</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Virgin Islands British</a>
+			</li>'
++'			<li>
+				<a href="http://caribbean.fujitsu.com/">Virgin Islands U.S.</a>
+			</li>'
 +'		</ul>'
 +'	</li>'
-+'	<li lang="en" xml:lang="en" class="cchanger-worldwide"><a class="deconone" href="http://www.fujitsu.com/uk/worldwide/"><img src="http://www.fujitsu.com/imgv4/common/icons/icon-worldwide.gif" width="16" height="16" alt="" /> Worldwide</a></li>'
-+'</ul>'
-
-
-);Activate_countryselector_action();
-//-->
++'	<li lang="en" xml:lang="en" class="cchanger-worldwide">
+		<a class="deconone" href="http://www.fujitsu.com/uk/worldwide/">
+			<img src="http://www.fujitsu.com/imgv4/common/icons/icon-worldwide.gif" width="16" height="16" alt=""/> Worldwide</a>
+	</li>'
++'</ul>
