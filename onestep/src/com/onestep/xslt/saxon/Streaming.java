63fca59db79b0ccb71bc651445641adc8ef39f76
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2011-03-24 22:35:32 +0000

    Streaming OK

--- onestep/src/com/onestep/xslt/saxon/Streaming.java
+++ onestep/src/com/onestep/xslt/saxon/Streaming.java
@@ -1,87 +1,57 @@
 package com.onestep.xslt.saxon;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.Result;
-import javax.xml.transform.Templates;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 
+import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-import org.xml.sax.helpers.XMLFilterImpl;
-import org.xml.sax.helpers.XMLReaderFactory;
 
-import com.onestep.xslt.reader.DecoHandler;
+import com.onestep.xslt.reader.CustomReader;
 
 public class Streaming {
 	public static void main(String[] args) {
 
 		String xmlSource = "com/onestep/xslt/saxon/profile-report.xml";
 		String[] xsltFiles = { "com/onestep/xslt/saxon/profile.xslt" };
-		
-		transform(xmlSource, xsltFiles);
 
+		transform(xmlSource, xsltFiles);
 
 	}
 
 	private static void transform(String xmlSource, String[] xsltFiles) {
 		// Pipe using transformerHandler
-		SAXTransformerFactory factory = (SAXTransformerFactory) TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", RunSaxon.class.getClassLoader());
-		File file = new File("file.txt");
+		SAXTransformerFactory factory = (SAXTransformerFactory) TransformerFactory.newInstance();
+		File file = new File("file.rtf");
 		System.out.println("Output File: " + file.getAbsolutePath());
-		Result result = new StreamResult((new File("file.txt")));
+		Result result = new StreamResult((new File("file.rtf")));
 
 		try {
-			XMLReader reader = XMLReaderFactory.createXMLReader();
-			XMLFilterImpl filter = new XMLFilterImpl(reader);
-			filter.setContentHandler(new DecoHandler(new DefaultHandler()));
-			
-			
-			InputStream stream = Streaming.class.getClassLoader().getResourceAsStream(xsltFiles[0]);
-			Templates template = factory.newTemplates(new StreamSource(stream));
-			Transformer transfomer = template.newTransformer();
-			if (xsltFiles.length == 1) {
-				stream = Streaming.class.getClassLoader().getResourceAsStream(xmlSource);
-				if (stream == null)
-					System.out.println("input source is null");
-				transfomer.transform(new StreamSource(stream),
-						result);
-			} else {
-				// create first handler
-				TransformerHandler handler = factory
-						.newTransformerHandler(new StreamSource(
-								new FileInputStream(xsltFiles[1])));
-				TransformerHandler firstHandler = handler;
-				// set the content handler to be the next handler
-				for (int i = 2; i < xsltFiles.length; i++) {
-					TransformerHandler nextHandler = factory
-							.newTransformerHandler(new StreamSource(
-									new FileInputStream(xsltFiles[i])));
-					handler.setResult(new SAXResult(nextHandler));
-					handler = nextHandler;
-				}
-				// last handler output is to the Result
-				handler.setResult(result);
 
-				// transform to the first handler
-				System.out.println("Pipe line using transfomer handler");
-				transfomer.transform(new StreamSource(new FileInputStream(
-						xmlSource)), new SAXResult(firstHandler));
+			SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+			saxFactory.setNamespaceAware(false);
+			// saxFactory.setValidating(false);
+			XMLReader xmlReader = saxFactory.newSAXParser().getXMLReader();
+			xmlReader = new CustomReader(xmlReader);
 
-			}
+			TransformerHandler handler = factory
+					.newTransformerHandler(new SAXSource(xmlReader, new InputSource(Streaming.class.getClassLoader().getResourceAsStream(xsltFiles[0]))));
 
+			handler.setResult(result);
+
+			InputStream stream = Streaming.class.getClassLoader().getResourceAsStream(xmlSource);
+			handler.getTransformer().transform(new SAXSource(new InputSource(stream)), result);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
