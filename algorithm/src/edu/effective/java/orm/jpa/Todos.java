26f2d8c5e45229dbc45c1febcbb211d4341bf8b7
mycguo <mycguo@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2012-01-31 21:39:14 +0000

    f

--- algorithm/src/edu/effective/java/orm/jpa/Todos.java
+++ algorithm/src/edu/effective/java/orm/jpa/Todos.java
@@ -1,38 +1,7 @@
 package edu.effective.java.orm.jpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Entity
+
 public class Todos {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String summary;
-	private String description;
-
-	public String getSummary() {
-		return summary;
-	}
-
-	public void setSummary(String summary) {
-		this.summary = summary;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	@Override
-	public String toString() {
-		return "Todo [summary=" + summary + ", description=" + description
-				+ "]";
-	}
 
 }
