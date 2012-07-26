21fd21dd81ca6d014935476b0593143adca1e977
mycguo@gmail.com <mycguo@gmail.com@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2012-07-26 23:52:14 +0000

    First Commit

---
+++ js/Node/index.js
@@ -0,0 +1,11 @@
+server = require('./server.js');
+router = require('./router.js');
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
