b13cce60e4025f39f992b87c1f381c8fe0beebaa
mycguo@gmail.com <mycguo@gmail.com@3efe49aa-c64d-0410-a8c4-6f330da110e3>
2012-08-21 20:29:30 +0000

    put it behind static

--- js/Node/app.js
+++ js/Node/app.js
@@ -17,8 +17,10 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
+
   app.use(express.static(__dirname + '/public'));
+  //put it below static
+  app.use(app.router);
 });
 
 app.configure('development', function(){
